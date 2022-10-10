38d32835beffa7da9cb84ffc4880c9cb6124223a
gimagoisk <115448364+gimagoisk@users.noreply.github.com>
2022-10-10 14:39:17 +0800

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>个人简历</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+
+
+<div id="page">
+	<section id="basic">
+		<h2>基本信息</h2>
+		<ul>
+			<li><span class="header">姓名：</span>尼基塔</li>
+			<li><span class="header">性别：</span>男</li>
+			<li><span class="header">出生年月：</span>2001-03-20</li>
+			<li><span class="header">籍贯：</span>阿拉木图</li>
+			<li><span class="header">政治面貌：</span>无</li>
+		</ul>
+
+	</section>
+
+	<section id="intro">
+		<h2>自我介绍</h2>
+
+		<p>大家好，我叫尼基塔。 我在哈萨克斯坦阿拉木图出生长大。 现在我是四年级的学生。 我参与过各种各样的项目，这些项目让我能够将我在课堂上学到的知识付诸实践。 我为自己注重细节、分析和驱动而感到自豪。
+		</p>
+	</section>
+
+	<section id="cityintro">
+		<h2>城市介绍</h2>
+		<p>阿拉木图，原名阿拉木图，是哈萨克斯坦最大的城市，人口约200万。 从 1929 年到 1936 年，它是哈萨克斯坦的首都，作为俄罗斯苏维埃联邦社会主义共和国内的一个自治共和国，作为苏联的一部分，然后从 1936 年到 1991 年作为一个加盟共和国，最后从 1991 年到 1997 年作为一个独立国家。
+阿拉木图仍然是哈萨克斯坦主要的商业和文化中心，也是其人口最多、国际化程度最高的城市。 这座城市位于哈萨克斯坦南部山区，靠近吉尔吉斯斯坦边境，位于外伊犁阿拉套山脚下，海拔700-900米，大小阿尔马廷卡河流入平原。
+		</p>
+	</section>
+
+	<section id="hobbies">
+    <h2>爱好清单</h2>
+	<table>
+			<tbody>
+				<tr>
+					<td>志愿服务和社区参与</td>
+				</tr>
+				<tr>
+					<td>学习语言</td>
+				</tr>
+				<tr>
+					<td>棋盘游戏和角色扮演</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+
+
+</div>
+</body>
+</html>
